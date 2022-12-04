3c26afa6572c99ed02fc44cf2c470eb48f06407b
Lenovo <lerapuisha@mail.ru>
2022-12-04 17:19:30 +0300

    Билет

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Задача по билету");
+
+        int i = 145;
+        System.out.println("Квадрат числа " + i + " равен " + i * i);
     }
 }
